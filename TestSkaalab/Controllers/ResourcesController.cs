663981d42a0244fe883a8fac88358cd9efc00297
Yerki <yerkinouceiba@gmail.com>
2023-08-11 18:06:14 +0100

    Add project files.

---
+++ TestSkaalab/Controllers/ResourcesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TestSkaalab.Context;
+using TestSkaalab.Models;
+
+namespace TestSkaalab.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        private ResourcesContext db = new ResourcesContext();
+
+
+ 
+        // GET: Resources
+        public ActionResult Index()
+        {
+            return View(db.Resources.ToList());
+        }
+
+        // GET: Resources/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resource resource = db.Resources.Find(id);
+            if (resource == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resource);
+        }
+
+        // GET: Resources/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Description,StartDate,EndDate")] Resource resource)
+        {
+            if (ModelState.IsValid)
+            {
+                resource.Id = Guid.NewGuid();
+                db.Resources.Add(resource);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(resource);
+        }
+
+        // GET: Resources/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resource resource = db.Resources.Find(id);
+            if (resource == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resource);
+        }
+
+        // POST: Resources/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Description,StartDate,EndDate")] Resource resource)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(resource).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(resource);
+        }
+
+        // GET: Resources/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resource resource = db.Resources.Find(id);
+            if (resource == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resource);
+        }
+
+        // POST: Resources/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Resource resource = db.Resources.Find(id);
+            db.Resources.Remove(resource);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TestSkaalab/Models/Resource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestSkaalab.Models
+{
+    public class Resource
+    {
+
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string  Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+    }
+}
